d34a87a5a8000db92f93eb8a848a89f1c6b4af88
shaharyar jameel <ms.jameel.02@gmail.com>
2024-03-21 09:55:58 -0700

    added a logout button

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,15 @@ document.getElementById("loginBtn").addEventListener("click", function() {
     window.location.href = "login.html";
 });
 
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+      }).catch((error) => {
+        // An error happened.
+      });
+}
+
