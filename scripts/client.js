3b034d8bf664111e553fb563666900f4b5b6e31b
Shayan Nikpour <shayannikpour2@gmail.com>
2024-03-27 19:36:09 -0700

    Added profile, meditation, and log out future

---
+++ scripts/client.js
@@ -0,0 +1,19 @@
+document.getElementById("sadcryIcon").addEventListener("click", function () {
+    handleIconClick(1);
+});
+
+document.getElementById("sadtearIcon").addEventListener("click", function () {
+    handleIconClick(2);
+});
+
+document.getElementById("mehIcon").addEventListener("click", function () {
+    handleIconClick(3);
+});
+
+document.getElementById("smileIcon").addEventListener("click", function () {
+    handleIconClick(4);
+});
+
+document.getElementById("laughIcon").addEventListener("click", function () {
+    handleIconClick(5);
+});
--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,13 @@
+
+
 function sayHello() {
     
 }
 //sayHello();
 
-document.getElementById("loginBtn").addEventListener("click", function() {
-    window.location.href = "login.html";
-});
+// document.getElementById("loginBtn").addEventListener("click", function() {
+//     window.location.href = "login.html";
+// });
 
 //------------------------------------------------
 // Call this function when the "logout" button is clicked
@@ -17,5 +19,53 @@ function logout() {
       }).catch((error) => {
         // An error happened.
       });
+
+      
 }
+
+document.getElementById('logout').addEventListener('click', function() {
+
+  Swal.fire({
+      position: "middle",
+      icon: "success",
+      title: "You've logged out succesfully!",
+      showConfirmButton: false,
+      timer: 1000
+  });
+
+  logout();
+
+
+  setTimeout(function() {
+      window.location.href = "login.html";
+  }, 1300);
+});
+
+
+
+//   const logoutBtn = document.querySelector('#sign-out');
+// logoutBtn.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   auth.signOut().then(() => {
+//     // Custom Modal from sweetalert
+//     Swal.fire({
+//       icon: 'success',
+//       title: 'Logout successful',
+//       showConfirmButton: false,
+//       timer: 1500 
+//     }).then(() => {
+//       window.location.href = "login.html";
+//     });
+//   });
+// });
+
+// Swal.fire({
+//   position: "top-end",
+//   icon: "success",
+//   title: "Your work has been saved",
+//   showConfirmButton: false,
+//   timer: 1500
+  
+// }.then(window.location.href = "login.html"));
+// Logout function
 
